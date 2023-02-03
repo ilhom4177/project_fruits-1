d59c781f43e83c92e262ff79449a278ab76f5024
ilhom <ilhommamatqulov276@gmail.com>
2023-02-03 15:08:59 +0500

    get_expensive_fruit.py

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -13,9 +13,9 @@ def get_expensive_fruit(data: str) -> str:
     price = 0
     for row in rows [1:]:
         fruit = row.split(',')
-    if price < float(fruit[-1]):
-        price = float(fruit[-1])
-        name = fruit [0]
+        if price < float(fruit[-1]):
+            price = float(fruit[-1])
+            name = fruit[0]
     return name
 data = open('fruits.csv').read()
 print(get_expensive_fruit(data)) 
